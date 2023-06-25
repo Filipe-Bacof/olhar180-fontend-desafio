405180dc80baa16e79a5607232d8b6edc7706e82
Filipe Bacof <filipebacof@gmail.com>
2023-06-25 20:16:18 -0300

    cors

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -3,6 +3,7 @@ import Cookies from 'js-cookie'
 
 export const Api: AxiosInstance = Axios.create({
   baseURL: 'https://olhar180-backend-desafio-production.up.railway.app',
+  withCredentials: true,
 })
 
 Api.interceptors.request.use(
